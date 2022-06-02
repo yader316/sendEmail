e84a62defbe3810b6b4c9687d636ab81a01c0e12
InterScopeDev <interscopedev@gmail.com>
2022-06-02 09:14:33 -0600

    se cambio el puerto a 5000

--- index.js
+++ index.js
@@ -74,8 +74,8 @@ app.post('/send-email', jsonParser, async (req, res) => {
     res.json({ message: 'Email has been sent', success: true });
 })
 
-const PORT = process.env.PORT || 3000;
-console.log({ PORT });
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT || 5000;
+console.log({ port });
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
